6ebfca1d65cbaa10d7714ea1e840166cfcdacd26
Magda Balun <magi@op.pl>
2018-02-16 12:22:03 +0100

    Format files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, hashHistory } from 'react-router';
-import { Provider } from 'react-redux';
-import store from './store/index';
-import routes from './routes';
+import React from "react";
+import { render } from "react-dom";
+import { Router, hashHistory } from "react-router";
+import { Provider } from "react-redux";
+import store from "./store/index";
+import routes from "./routes";
 import DevTools from "./DevTools";
-import './country.css';
+import "./country.css";
 
 render(
-    <Provider store={store}>
-    	<div>
-        	<Router history={hashHistory} routes={routes}/>
-        	<DevTools />
-     	</div>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <div>
+      <Router history={hashHistory} routes={routes} />
+      <DevTools />
+    </div>
+  </Provider>,
+  document.getElementById("root")
 );
